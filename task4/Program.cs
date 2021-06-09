5e4299628e7a0e66091853c718f34134c489d849
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-06-09 19:13:25 +0300

    Task 4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace task4
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace task4
 {
+    class MyComparer<T> : Comparer<T>
+    {
+        public override int Compare(T x, T y)
+        {
+            return y.GetHashCode() - x.GetHashCode();
+        }
+    }
+
     internal static class Program
     {
         private static void Main()
         {
             //Создайте коллекцию типа OrderedDictionary и реализуйте в ней возможность сравнения значений ключей.
+            var collection = new OrderedDictionary {{"1", "a"}, {"2", "b"}};
+            var list = new List<int>()
+                {
+                    1, 2, 3
+                }
+                ;
+
+            var comparer = new MyComparer<int>();
+            list.Sort(comparer);
         }
     }
 }
